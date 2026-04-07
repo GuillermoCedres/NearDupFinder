9da6ce4a67e96c5d350579f95fdddc57319ef8cd
GuillermoCedres <GC306073@fi365.ort.edu.uy>
2026-04-07 04:34:11 -0300

    TDD-Red

--- Domain.Tests/ItemTests.cs
+++ Domain.Tests/ItemTests.cs
@@ -3,5 +3,10 @@ namespace Domain.Tests;
 [TestClass]
 public class ItemTests
 {
-    
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ShouldThrowException_WhenItemTitleIsEmpty()
+    {
+        var item = new Item(""); 
+    }
 }
